3a240a26ce1f53d417d63f72fb38adff0c4545bc
Bernard Desert <bdesert@etsy.com>
2016-06-04 14:04:44 -0400

    item view controller

--- inventorymanagement/InventoryItemViewController.m
+++ inventorymanagement/InventoryItemViewController.m
@@ -38,82 +38,84 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    int startingYPos = 160;
+    int yDelta = 20;
+
+    
     self.title = self.inventoryItem.title;
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    // I feel this is too many UILabels
-    // Would like to move these in order of how they are displayed
-    // title , location , purchase , current ,
-    // our $$ values are not showing when clinking an item autogenerator not working
-    // arc4random ^ 
 
     UILabel *titleLabel = [[UILabel alloc] init];
     titleLabel.frame = CGRectMake(20, 80, self.view.bounds.size.width - 40, 45);
     titleLabel.textColor = [UIColor darkTextColor];
     titleLabel.text = self.inventoryItem.title;
     [self.view addSubview:titleLabel];
-    
+
     UILabel *locationLabel = [[UILabel alloc] init];
     locationLabel.frame = CGRectMake(20, 112, self.view.bounds.size.width - 40, 30);
     locationLabel.textColor = [UIColor darkTextColor];
     locationLabel.font = [UIFont systemFontOfSize:15];
     locationLabel.text = self.inventoryItem.location ? @"Has location" : @"No location";
     [self.view addSubview:locationLabel];
-        
+    
+    if ([self.inventoryItem conformsToProtocol:@protocol(ValuableItem)]) {
     UILabel *purchasedValueLabel = [[UILabel alloc] init];
-    purchasedValueLabel.frame = CGRectMake(20, 300, self.view.bounds.size.width - 40, 30);
+    purchasedValueLabel.frame = CGRectMake(20, startingYPos, self.view.bounds.size.width - 40, 30);
     purchasedValueLabel.textColor = [UIColor darkTextColor];
     purchasedValueLabel.font = [UIFont systemFontOfSize:15];
     [self.view addSubview:purchasedValueLabel];
-  
+
+    if ([self.inventoryItem conformsToProtocol:@protocol(ValuableItem)]) {
     UILabel *currentValueLabel = [[UILabel alloc] init];
-    currentValueLabel.frame = CGRectMake(20, 250, self.view.bounds.size.width - 40, 30);
+    currentValueLabel.frame = CGRectMake(20, startingYPos, self.view.bounds.size.width - 40, 30);
     currentValueLabel.textColor = [UIColor darkTextColor];
     currentValueLabel.font = [UIFont systemFontOfSize:15];
     [self.view addSubview:currentValueLabel];
-    
-/*  BOOL for Framed
-    
+        
+    }
+        startingYPos = startingYPos + yDelta;
+        
+    }
+
+/*
+    if ([self.inventoryItem conformsToProtocol:@protocol(Framable)]) {
     UILabel *framedLabel = [[UILabel alloc] init];
     framedLabel.frame = CGRectMake(20, 112, self.view.bounds.size.width - 40, 30);
     framedLabel.textColor = [UIColor darkTextColor];
     framedLabel.font = [UIFont systemFontOfSize:15];
-    framedLabel.text = self.inventoryItem.? @"Has location" : @"No location";
-    [self.view addSubview:framedLabel];
-    
- */
-    
-    
-    // do i need this if statement like I do for Physical see line 112
-    
+    // should it be a BOOL or a swtich
+    // do i need to update the property to have values like Physical ENUM
+    // locationLabel.text = self.inventoryItem. ? @"Has location" : @"No location";
+*/
     if ([self.inventoryItem conformsToProtocol:@protocol(Size)]) {
-        id <Size> sizeItem = (id <Size>)self.inventoryItem;
-        UILabel *label = [[UILabel alloc] init];
-        label.frame = CGRectMake(20, 200, self.view.bounds.size.width - 40, 30);
-        label.textColor = [UIColor darkTextColor];
-        label.font = [UIFont systemFontOfSize:15];
+    id <Size> sizeItem = (id <Size>)self.inventoryItem;
+    UILabel *label = [[UILabel alloc] init];
+    label.frame = CGRectMake(20, startingYPos, self.view.bounds.size.width - 40, 30);
+    label.textColor = [UIColor darkTextColor];
+    label.font = [UIFont systemFontOfSize:15];
         
         switch (sizeItem.height) {
             case StatueFigureSizeSmall:
-                label.text = @"Small";
-                break;
+            label.text = @"Small";
+            break;
             case StatueFigureSizeLarge:
-                label.text = @"Large";
-                break;
+            label.text = @"Large";
+            break;
             case StatueFigureSizeBust:
-                label.text = @"Bust";
-                break;
-                
+            label.text = @"Bust";
+            break;
+    
         }
         
-        [self.view addSubview:label];
+        startingYPos = startingYPos + yDelta;
         
+        [self.view addSubview:label];
     }
 
     if ([self.inventoryItem conformsToProtocol:@protocol(Physical)]) {
     id <Physical> physicalItem = (id <Physical>)self.inventoryItem;
     UILabel *label = [[UILabel alloc] init];
-    label.frame = CGRectMake(20, 160, self.view.bounds.size.width - 40, 30);
+    label.frame = CGRectMake(20, startingYPos, self.view.bounds.size.width - 40, 30);
     label.textColor = [UIColor darkTextColor];
     label.font = [UIFont systemFontOfSize:15];
         
@@ -123,13 +125,14 @@
             break;
             case PhysicalMaterialCeramic:
             label.text = @"Ceramic";
-            break;
-              
-      }
+           break;
+ 
+    }
+        
+    startingYPos = startingYPos + yDelta;
         
         [self.view addSubview:label];
- 
-}
+    }
  
 }
 
