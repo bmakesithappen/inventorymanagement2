2c8fbdbd5a6a7b291a16079b7767fdfc7e5b94d1
Bernard Desert <bdesert@etsy.com>
2016-06-21 15:19:48 -0400

    kif tests

---
+++ AcceptanceTests/InventoryViewTest.m
@@ -0,0 +1,45 @@
+//
+//  InventoryViewTest.m
+//  inventorymanagement
+//
+//  Created by Bernard Desert on 06/13/2016.
+//  Copyright © 2016 Bernard Desert. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
+
+@interface InventoryViewTest : KIFTestCase
+
+@end
+
+@implementation InventoryViewTest
+
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    
+    [tester tapViewWithAccessibilityLabel:@"YasinGayeTheArrival"];
+    
+    // I do not have any functions in my app that would apply to the below:
+    
+   // [tester waitForTappableViewWithAccessibilityLabel:@""];
+    
+   // [tester enterText:<#(NSString *)#> intoViewWithAccessibilityLabel:<#(NSString *)#>
+
+    
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+
+@end
---
+++ inventorymanagementTests/inventorymanagementTests.m
@@ -0,0 +1,39 @@
+//
+//  inventorymanagementTests.m
+//  inventorymanagementTests
+//
+//  Created by Bernard Desert on 06/13/2016.
+//  Copyright © 2016 Bernard Desert. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface inventorymanagementTests : XCTestCase
+
+@end
+
+@implementation inventorymanagementTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
